b173711acc6263df35ee3abe67e64adbdfb2ae1e
wenyan4work <wenyan4work@gmail.com>
2020-01-14 19:54:39 -0500

    bugfix

--- Compile.py
+++ Compile.py
@@ -1,33 +1,38 @@
 import os
+import multiprocessing
 
 # sets environment variables
 os.environ["SFTPATH"] = os.environ["HOME"]+"/local"
 print("install destination:", os.environ["SFTPATH"])
 # intel compiler
-os.environ["CXXFLAGS"] = "-O3 -xHost -DNDEBUG -qno-offload"
-os.environ["CFLAGS"] = "-O3 -xHost -DNDEBUG -qno-offload"
-os.environ["OPENMP_CXX_FLAGS"] = "-qopenmp"
-os.environ["OPENMP_C_FLAGS"] = "-qopenmp"
+# os.environ["CXXFLAGS"] = "-O3 -xHost -DNDEBUG -qno-offload"
+# os.environ["CFLAGS"] = "-O3 -xHost -DNDEBUG -qno-offload"
+# os.environ["OPENMP_CXX_FLAGS"] = "-qopenmp"
+# os.environ["OPENMP_C_FLAGS"] = "-qopenmp"
 
 # gcc
-# os.environ["CXXFLAGS"] = "-O3 -march=native -DNDEBUG"
-# os.environ["CFLAGS"] = "-O3 -march=native -DNDEBUG"
-# os.environ["OPENMP_CXX_FLAGS"] = "-fopenmp"
-# os.environ["OPENMP_C_FLAGS"] = "-fopenmp"
+os.environ["CXXFLAGS"] = "-O3 -march=native -DNDEBUG"
+os.environ["CFLAGS"] = "-O3 -march=native -DNDEBUG"
+os.environ["OPENMP_CXX_FLAGS"] = "-fopenmp"
+os.environ["OPENMP_C_FLAGS"] = "-fopenmp"
 
 install = False
 check_eigen = False
 test_Trilinos = True
+make_jobs = multiprocessing.cpu_count()
 
 
 cwd = os.getcwd()
+
+os.system('git submodule init')
+os.system('git submodule update')
 
 # TRNG
 os.chdir(cwd)
 os.chdir('TRNG')
 os.system('rm -rf ./build && mkdir ./build')
 os.chdir('build')
-os.system('bash ../do-configure-TRNG4.sh && make -j8')
+os.system('bash ../do-configure-TRNG4.sh && make -j'+str(make_jobs))
 os.system('./examples/time')
 if install:
     os.system('make install')
@@ -37,7 +42,7 @@ os.chdir(cwd)
 os.chdir('YamlCpp')
 os.system('rm -rf ./build && mkdir ./build')
 os.chdir('build')
-os.system('bash ../do-configure-YamlCpp.sh && make -j8')
+os.system('bash ../do-configure-YamlCpp.sh && make -j'+str(make_jobs))
 if install:
     os.system('make install')
 
@@ -46,7 +51,7 @@ os.chdir(cwd)
 os.chdir('MsgPack')
 os.system('rm -rf ./build && mkdir ./build')
 os.chdir('build')
-os.system('bash ../do-configure-MsgPack.sh && make -j8')
+os.system('bash ../do-configure-MsgPack.sh && make -j'+str(make_jobs))
 if install:
     os.system('make install')
 
@@ -55,7 +60,7 @@ os.chdir(cwd)
 os.chdir('Eigen')
 os.system('rm -rf ./build && mkdir ./build')
 os.chdir('build')
-os.system('bash ../do-configure-Eigen.sh && make -j8')
+os.system('bash ../do-configure-Eigen.sh && make -j'+str(make_jobs))
 if check_eigen:
     os.system('make check -j8')
 if install:
@@ -67,7 +72,7 @@ os.chdir(cwd)
 os.chdir('Trilinos')
 os.system('rm -rf ./build && mkdir ./build')
 os.chdir('build')
-os.system('bash ../do-configure-Trilinos.sh && make -j8')
+os.system('bash ../do-configure-Trilinos.sh && make -j'+str(make_jobs))
 if test_Trilinos:
     os.environ["OMP_NUM_THREADS"] = "4"
     os.system('make test')
--- README.md
+++ README.md
@@ -3,6 +3,10 @@ A simple collection of necessary external libraries for my simulation
 
 Easy steps:
 - `git clone`, `git submodule init` and `git submodule update` to download those software
-- modify Compile.py for your compiler and cpu settings
+- modify Compile.py for your compiler and cpu settings, and choose if install or not
 - `python3 Compile.py` to compile the library
 - go to `PVFMM/pvfmm` and using the provided `do-configure.sh` to compile manually if `pvfmm` is necessary
+
+Dependency:
+- `gcc>=7` for full `c++14` support
+- `mpicxx` and `mpicc`
