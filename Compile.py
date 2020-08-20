f965e6b8f22d88326e3adf2755b9f298b0918be7
wenyan4work@gmail.com <wenyan4work@gmail.com>
2020-08-20 10:41:25 -0400

    add minimal vtk

--- Compile.py
+++ Compile.py
@@ -17,6 +17,17 @@ os.environ["CFLAGS"] = "-O3 -march=native -DNDEBUG"
 os.environ["OPENMP_CXX_FLAGS"] = "-fopenmp"
 os.environ["OPENMP_C_FLAGS"] = "-fopenmp"
 
+# comment out component you don't want
+enable = [
+    'trng',
+    'eigen',
+    'msgpack',
+    'yamlcpp',
+    'trilinos',
+    'pvfmm',
+    'vtk'
+]
+
 install = True
 check_eigen = False
 test_Trilinos = True
@@ -30,63 +41,79 @@ os.system('git submodule init')
 os.system('git submodule update')
 
 # TRNG
-os.chdir(cwd)
-os.chdir('TRNG')
-os.system('rm -rf ./build && mkdir ./build')
-os.chdir('build')
-os.system('bash ../do-configure-TRNG4.sh && make -j'+str(make_jobs))
-os.system('./examples/time')
-if install:
-    os.system('make install')
+if 'trng' in enable:
+    os.chdir(cwd)
+    os.chdir('TRNG')
+    os.system('rm -rf ./build && mkdir ./build')
+    os.chdir('build')
+    os.system('bash ../do-configure-TRNG4.sh && make -j'+str(make_jobs))
+    os.system('./examples/time')
+    if install:
+        os.system('make install')
 
 # YamlCpp
-os.chdir(cwd)
-os.chdir('YamlCpp')
-os.system('rm -rf ./build && mkdir ./build')
-os.chdir('build')
-os.system('bash ../do-configure-YamlCpp.sh && make -j'+str(make_jobs))
-if install:
-    os.system('make install')
+if 'yamlcpp' in enable:
+    os.chdir(cwd)
+    os.chdir('YamlCpp')
+    os.system('rm -rf ./build && mkdir ./build')
+    os.chdir('build')
+    os.system('bash ../do-configure-YamlCpp.sh && make -j'+str(make_jobs))
+    if install:
+        os.system('make install')
 
 # MsgPack
-os.chdir(cwd)
-os.chdir('MsgPack')
-os.system('rm -rf ./build && mkdir ./build')
-os.chdir('build')
-os.system('bash ../do-configure-MsgPack.sh && make -j'+str(make_jobs))
-if install:
-    os.system('make install')
+if 'msgpack' in enable:
+    os.chdir(cwd)
+    os.chdir('MsgPack')
+    os.system('rm -rf ./build && mkdir ./build')
+    os.chdir('build')
+    os.system('bash ../do-configure-MsgPack.sh && make -j'+str(make_jobs))
+    if install:
+        os.system('make install')
 
 # Eigen
-os.chdir(cwd)
-os.chdir('Eigen')
-os.system('rm -rf ./build && mkdir ./build')
-os.chdir('build')
-os.system('bash ../do-configure-Eigen.sh && make -j'+str(make_jobs))
-if check_eigen:
-    os.system('make check -j8')
-if install:
-    os.system('make install')
+if 'eigen' in enable:
+    os.chdir(cwd)
+    os.chdir('Eigen')
+    os.system('rm -rf ./build && mkdir ./build')
+    os.chdir('build')
+    os.system('bash ../do-configure-Eigen.sh && make -j'+str(make_jobs))
+    if check_eigen:
+        os.system('make check -j8')
+    if install:
+        os.system('make install')
 
 
 # Trilinos
-os.chdir(cwd)
-os.chdir('Trilinos')
-os.system('rm -rf ./build && mkdir ./build')
-os.chdir('build')
-os.system('bash ../do-configure-Trilinos.sh && make -j'+str(make_jobs))
-if test_Trilinos:
-    os.environ["OMP_NUM_THREADS"] = "3"
-    os.system('make test')
-if install:
-    os.system('make install')
+if 'trilinos' in enable:
+    os.chdir(cwd)
+    os.chdir('Trilinos')
+    os.system('rm -rf ./build && mkdir ./build')
+    os.chdir('build')
+    os.system('bash ../do-configure-Trilinos.sh && make -j'+str(make_jobs))
+    if test_Trilinos:
+        os.environ["OMP_NUM_THREADS"] = "3"
+        os.system('make test')
+    if install:
+        os.system('make install')
 
 # PVFMM
-os.chdir(cwd)
-os.chdir('PVFMM')
-os.system('rm -rf ./build && mkdir ./build')
-os.chdir('build')
-os.system('bash ../do-configure-pvfmm.sh && make -j'+str(make_jobs))
-os.system('./examples/example1 -N 65536 -omp 4')
-if install:
-    os.system('make install')
+if 'pvfmm' in enable:
+    os.chdir(cwd)
+    os.chdir('PVFMM')
+    os.system('rm -rf ./build && mkdir ./build')
+    os.chdir('build')
+    os.system('bash ../do-configure-pvfmm.sh && make -j'+str(make_jobs))
+    os.system('./examples/example1 -N 65536 -omp 4')
+    if install:
+        os.system('make install')
+
+# VTK
+if 'vtk' in enable:
+    os.chdir(cwd)
+    os.chdir('VTK')
+    os.system('rm -rf ./build && mkdir ./build')
+    os.chdir('build')
+    os.system('bash ../do-configure-VTK.sh && make -j'+str(make_jobs))
+    if install:
+        os.system('make install')
